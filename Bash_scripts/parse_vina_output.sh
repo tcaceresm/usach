675b94dbd4709d35cab151f1f1d21d2738690af2
tcaceres <caceres.tomas99@gmail.com>
2024-04-23 17:39:20 -0400

    Removed txt extension to facilitate posterior processing

--- Bash_scripts/parse_vina_output.sh
+++ Bash_scripts/parse_vina_output.sh
@@ -42,7 +42,7 @@ declare -a LIGANDS=($(sed "s/.pdbqt//g" <<< "${LIGANDS_PDBQT[*]}"))
 for LIGAND in "${LIGANDS[@]}"
  do
   echo "Summary file path ${SUMMARY_FILE_PATH}"
-  grep -i "result" -m 1 "${LIGANDS_PDBQT_PATH}/${LIGAND}.pdbqt" | awk '{print $4}'> "${SUMMARY_FILE_PATH}/${LIGAND}.txt"
+  grep -i "result" -m 1 "${LIGANDS_PDBQT_PATH}/${LIGAND}.pdbqt" | awk '{print $4}'> "${SUMMARY_FILE_PATH}/${LIGAND}"
 
  done
 
