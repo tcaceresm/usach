671cbc21893fc0e4ad7655d2a9c3148eca69bb33
tcaceres <caceres.tomas99@gmail.com>
2024-04-23 15:46:27 -0400

    Prepared Bash scripts to prepare ligand pdbqt file and run AutoDock Vina for multiple ligands

--- Bash_scripts/prepare_pdbqt.sh
+++ Bash_scripts/prepare_pdbqt.sh
@@ -1,14 +1,54 @@
 #!/usr/bin/bash
 
-ligands_path="/home/pc-usach-cm/Escritorio/tcaceres/docking/corr_hinokiflavone/to_docking/ligandos"
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
 
-declare -a ligandos_a_convertir=($(ls ${ligands_path}))
+   echo "Syntax: [-h|d|e]"
+   echo "To save a log file and also print the status, run: bash prepare_odbqt.sh -d \$DIRECTORY | tee -a \$LOGFILE"
+   echo "Options:"
+   echo "h     Print help"
+   echo "d     Ligands SDF path"
+   echo "e     Ligands PDBQT output path"
+   
+}
 
-for ligando_a_convertir in "${ligandos_a_convertir[@]}"
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+
+while getopts ":hde:" option; do
+   case $option in
+      h) # Print this help
+         Help
+         exit;;
+      d) # Enter the Ligands SDF directory path
+         LIGANDS_SDF_PATH=$OPTARG;;
+      e) # Enter the Ligands PDBQT output path
+         LIGANDS_PDBQT_PATH=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+# This script path
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Ligands SDF format
+declare -a LIGANDS_SDF=($(ls ${LIGANDS_SDF_PATH}/))
+# We replace .sdf with nothing. It's easier to add PDBQT extension.
+declare -a LIGANDS=($(sed "s/.sdf//g" <<< "${LIGANDS_SDF[*]}"))
+
+
+for LIGAND in "${LIGANDS[@]}"
    do
-    echo "Convirtiendo ${ligando_a_convertir} a pdbqt"
-    /home/pc-usach-cm/.local/bin/mk_prepare_ligand.py -i ${ligands_path}/${ligando_a_convertir} -o "${ligands_path}/${ligando_a_convertir}.pdbqt"
-    echo "Listo para ${ligando_a_convertir}"
+    echo "Converting ${LIGAND} to PDBQT format"
+    /home/pc-usach-cm/.local/bin/mk_prepare_ligand.py -i '${LIGANDS_SDF_PATH}/${LIGAND}.sdf' -o "${LIGANDS_PDBQT_PATH}/${LIGAND}.pdbqt"
+    echo "Done: ${LIGAND}"
    done
    
-rename 's/.sdf.pdbqt/.pdbqt/' ${ligands_path}/*.sdf.pdbqt   
--- Bash_scripts/run_vina.sh
+++ Bash_scripts/run_vina.sh
@@ -1,11 +1,55 @@
 #!/usr/bin/bash
 
-config_file='/home/pc-usach-cm/Escritorio/tcaceres/docking/autodock_vina_input/grid_box_monomers.txt'
-ligands_path='/home/pc-usach-cm/Escritorio/tcaceres/docking/corr_hinokiflavone/to_docking/ligandos'
-declare -a ligands=($(ls ${ligands_path}))
-output_path='/home/pc-usach-cm/Escritorio/tcaceres/docking/corr_hinokiflavone/to_docking/vina_out'
-for ligand in "${ligands[@]}"
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+
+   echo "Syntax: [-hrlco]"
+   echo "To save a log file and also print the status, run: bash prepare_odbqt.sh -d \$DIRECTORY | tee -a \$LOGFILE"
+   echo "Options:"
+   echo "h     Print help"
+   echo "r     Receptor PDBQT file"
+   echo "l     Ligands PDBQT"
+   echo "c     Configuration file. It should contain grid box information, Num_modes and exhaustiveness"
+   echo "o     Output directory"
+   
+}
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+
+while getopts ":hrlco:" option; do
+   case $option in
+      h) # Print this help
+         Help
+         exit;;
+      r) # Receptor PDBQT file
+         RECEPTOR_PDBQT=$OPTARG;;
+      l) # Enter the Ligands PDBQT output path
+         LIGANDS_PDBQT_PATH=$OPTARG;;
+      c) # Vina configuration file
+         CONFIG_FILE=$OPTARG;;
+      o) # Output path
+         OUTPUT_PATH=$OPTARG;;   
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+declare -a LIGANDS=($(ls ${LIGANDS_PDBQT_PATH}))
+
+
+for LIGAND  in "${LIGANDS[@]}"
  do
   echo "Docking ${ligand}"
-  /home/pc-usach-cm/Documentos/autodock_vina_1_1_2_linux_x86/bin/vina --config ${config_file} --ligand ${ligands_path}/${ligand} --out ${output_path}/${ligand}
+  /home/pc-usach-cm/Documentos/autodock_vina_1_1_2_linux_x86/bin/vina --config ${CONFIG_FILE} --ligand ${LIGANDS_PDBQT_PATH}/${LIGAND} --out ${OUTPUT_PATH}/${LIGAND}
  done
+
+echo "Done!"
