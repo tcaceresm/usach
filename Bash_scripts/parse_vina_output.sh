8b646c2b81552dc288f94dded2cb3f99bf4fdb4c
tcaceres <caceres.tomas99@gmail.com>
2024-04-23 17:02:41 -0400

    fixed arguments

--- Bash_scripts/parse_vina_output.sh
+++ Bash_scripts/parse_vina_output.sh
@@ -37,7 +37,9 @@ while getopts ":heo:" option; do
 done
 
 echo "peo"
+echo $LIGANDS_PDBQT_PATH
 declare -a LIGANDS_PDBQT=($(ls ${LIGANDS_PDBQT_PATH}))
+
 echo "ligands pdbqt ${LIGANDS_PDBQT}"
 declare -a LIGANDS=($(sed "s/.pdbqt//g" <<< "${LIGANDS_PDBQT[*]}"))
 
