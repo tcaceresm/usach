f4abf5535469aeb12654e44b5256a457c8c2b9ea
tcaceresm <caceres.tomas99@gmail.com>
2024-07-11 11:35:45 -0400

    updated file

---
+++ scripts/dlg2pdbqt.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+############################################
+# Transformación de formato .dlg a .pdbqt  #
+############################################
+
+############################################################
+# Help
+############################################################
+Help() {
+    echo "Syntax: ordenar_pdb.sh [-h|d|f|o]"
+    echo "To save a log file and also print the status, run: ordenar_pdb.sh -d \$DIRECTORY | tee -a \$LOGFILE"
+    echo "Options:"
+    echo "h     Print help"
+    echo "d     Input directory containing dlg files."
+    echo "f     Output directory."
+}
+
+while getopts ":hd:i:f:o:" option; do
+    case $option in
+        h)  # Print this help
+            Help
+            exit;;
+        d)  # Enter the input directory
+            IPATH=$OPTARG;;
+        f)  # Output directory
+            OPATH=$OPTARG;;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+            exit;;
+    esac
+done
+
+for DLG_FILE in "$IPATH"/*.dlg; do
+
+    LIGAND_PDBQT=$(basename $DLG_FILE .dlg)
+
+    echo "Converting $DLG_FILE to $LIGAND_PDBQT"
+
+    # Archivo temporal para almacenar los valores de energía y los nombres de conformaciones
+    CONFORMATIONS_DIR="${OPATH}/${LIGAND_PDBQT}/"
+    # Crear un directorio temporal para almacenar las conformaciones
+    mkdir -p $CONFORMATIONS_DIR
+
+    grep 'DOCKED' $DLG_FILE > $LIGAND_PDBQT
+    grep -v 'FINAL DOCKET STATE' $LIGAND_PDBQT > temp.pdbqt && mv temp.pdbqt $LIGAND_PDBQT
+    sed -i 's/DOCKED: //g' $LIGAND_PDBQT
+    sed -i 's/USER/REMARK/g' $LIGAND_PDBQT
+    grep -v 'MODEL' $LIGAND_PDBQT > temp.pdbqt && mv temp.pdbqt $LIGAND_PDBQT
+    grep -v '^DOCKED: USER\s*_' $LIGAND_PDBQT > temp.pdbqt && mv temp.pdbqt $LIGAND_PDBQT
+    grep -v '^DOCKED: USER\s*x\s*y\s*z' $LIGAND_PDBQT > temp.pdbqt && mv temp.pdbqt $LIGAND_PDBQT
+
+    echo "Converted $DLG_FILE to $LIGAND_PDBQT"
+
+done
