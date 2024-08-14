777289c4384aa90b05ae801cd096c2295b6c7bfc
tcaceresm <caceres.tomas99@gmail.com>
2024-08-14 17:54:09 -0400

    updated file

---
+++ scripts/process_docking/run_clustering.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+
+############################################################
+# Help
+############################################################
+Help() {
+    echo "Syntax: run_clustering.sh [-h|d|f|o]"
+    echo "To save a log file and also print the status, run: cluster.sh -d \$DIRECTORY | tee -a \$LOGFILE"
+    echo "Options:"
+    echo "h     Print help"
+    echo "c     RMSD cutoff."
+    echo "d     Input directory containing dlg folder."
+    echo "n     Output folder containing processed folder of ligands."
+}
+
+while getopts ":hc:d:n:" option; do
+    case $option in
+        h)  # Print this help
+            Help
+            exit;;
+        c)  # RMSD cutoff
+            CUTOFF=$OPTARG;;
+        d)  # Enter the input directory
+            IPATH=$OPTARG;;
+        n)  # Processed dlg folder
+            PROCESSED_DLG=$OPTARG;;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+            exit;;
+    esac
+done
+
+for DLG_FILE in "$IPATH"/*.dlg; do
+
+    LIGAND_PDBQT=$(basename $DLG_FILE .dlg)
+    LIGAND_NAME=$(basename $LIGAND_PDBQT .pdbqt)
+
+    SDF_DIR="${IPATH}/${PROCESSED_DLG}/${LIGAND_NAME}/sdf"
+    PDB_DIR="${IPATH}/${PROCESSED_DLG}/${LIGAND_NAME}/pdb"
+
+    rmsd_df_path=${SDF_DIR}/${LIGAND_NAME}_RMSD_matrix.data
+    docking_scores=${PDB_DIR}/${LIGAND_NAME}_scores.csv
+    sdf_path=${SDF_DIR}/${LIGAND_NAME}_sorted_conformations.sdf
+    output_path=${SDF_DIR}/cluster/
+
+    Rscript $rmsd_df_path $docking_scores $cutoff $sdf $output_path
+    
+done
