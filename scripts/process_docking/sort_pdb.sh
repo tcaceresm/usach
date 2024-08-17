fe97d0ede9f160b86e99a27650b6d74f0b45b374
tcaceresm <caceres.tomas99@gmail.com>
2024-08-16 23:21:11 -0400

    updated file

--- scripts/process_docking/sort_pdb.sh
+++ scripts/process_docking/sort_pdb.sh
@@ -41,65 +41,66 @@ for DLG_FILE in "$IPATH"/*.dlg; do
     LIGAND_PDBQT=$(basename $DLG_FILE .dlg)
     LIGAND_NAME=$(basename $LIGAND_PDBQT .pdbqt)
     echo "Doing for $LIGAND_NAME"    
+
     # Archivo temporal para almacenar los valores de energía y los nombres de conformaciones
     CONFORMATIONS_DIR="${IPATH}/data/${LIGAND_NAME}/pdb"
-    TEMP_FILE="${IPATH}/data/${LIGAND_NAME}/pdb/temp_energy_values.txt"
+    ENERGY_FILE="${IPATH}/data/${LIGAND_NAME}/pdb/docking_energies.txt"
     PDB_FILE=${CONFORMATIONS_DIR}/$LIGAND_NAME.pdb
-    
+        
     # Crear un directorio temporal para almacenar las conformaciones
     mkdir -p $CONFORMATIONS_DIR
 
-    # Limpiar el archivo temporal si existe
-    > $TEMP_FILE
+    # Split pdb file, obtain energy values  
 
-    # Variables para manejar las conformaciones
-    model_num=""
-    conformation=""
-    energy=""
+    awk -v energy_file=${ENERGY_FILE} -v conformation_dir=${CONFORMATIONS_DIR} '
+    /MODEL/ {
+        molecule = $0 "\n"
+        run = $2
+        out = (conformation_dir "/model_" run ".pdb")
+        }
+    /Estimated Free Energy of Binding/ {
+        molecule = (molecule $0 "\n")
+        energy = $8; (molecule $0 "\n")
+        }
+    /ENDMDL/ {
+        molecule = (molecule $0 "\n")
+        print molecule > out
+        close(out)
+        print (energy, run) > energy_file
+    }
 
-    # Leer el archivo PDB línea por línea
-    while IFS= read -r line; do
-        # Buscar el inicio de una nueva conformación
-        if [[ "$line" =~ ^MODEL ]]; then
-            model_num=$(echo "$line" | awk '{print $2}')
-            conformation="$line"$'\n'
-        elif [[ "$line" =~ ^ENDMDL ]]; then
-            conformation+="$line"$'\n'
-            # Guardar la conformación en un archivo temporal
-            echo "$energy $model_num" >> $TEMP_FILE
-            echo -e "$conformation" > "$CONFORMATIONS_DIR/model_$model_num.pdb"
-            conformation=""
-        elif [[ "$line" =~ ^REMARK.*Estimated\ Free\ Energy\ of\ Binding ]]; then
-            energy=$(echo "$line" | awk '{print $8}')
-            conformation+="$line"$'\n'
-        else
-            conformation+="$line"$'\n'
-        fi
-    done < "$PDB_FILE"
+    !(/MODEL/ || /Estimated Free Energy of Binding/ || /ENDMDL/) {
+        molecule = molecule $0 "\n"
+    }
+    ' $PDB_FILE
+
+
+    if test -f "$ENERGY_FILE"; then
+        :
+    else
+        echo "Energy File not generated!. Please check ${LIGAND_NAME}"
+        
+    fi
 
     # Ordenar las conformaciones por energía
-    sort -n $TEMP_FILE -o $TEMP_FILE
+    sort -n $ENERGY_FILE -o $ENERGY_FILE
 
     # Crear un archivo final con las conformaciones ordenadas
     SORTED_FILE="$CONFORMATIONS_DIR/${LIGAND_NAME}_sorted_conformations.pdb"
     > $SORTED_FILE
 
-    # Leer el archivo temporal y concatenar las conformaciones ordenadas
+    # Leer el archivo de energías y concatenar las conformaciones ordenadas
     while IFS= read -r line; do
         model_num=$(echo "$line" | awk '{print $2}')
         cat "$CONFORMATIONS_DIR/model_$model_num.pdb" >> $SORTED_FILE
-    done < $TEMP_FILE
+    done < $ENERGY_FILE
 
     echo "Sorted PDB!"
-
-    # Mostrar el archivo ordenado
-    # cat $SORTED_FILE
 
     # Limpiar archivos temporales
     rm -rf $CONFORMATIONS_DIR/model_*.pdb
 
     # Obtener SDF con poses ordenadas
-
 
     echo "Generating sorted SDF file based on sorted PDB"
     obabel -ipdb "$CONFORMATIONS_DIR/${LIGAND_NAME}_sorted_conformations.pdb" -osdf -O"${IPATH}/data/${LIGAND_NAME}/sdf/${LIGAND_NAME}_sorted_conformations.sdf"
