11efa840de62b7d74a1a5a00726ca7de73b09246
tcaceresm <caceres.tomas99@gmail.com>
2024-10-14 16:38:05 -0300

    updated file

--- scripts/run_docking/run_vs.sh
+++ scripts/run_docking/run_vs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# Simple script to perform docking of several ligands in different pockets
 
 ############################################################
 # Help                                                     #
@@ -12,9 +13,9 @@ Help()
    echo "To save a log file and also print the status, run: bash prepare_odbqt.sh -d \$DIRECTORY | tee -a \$LOGFILE"
    echo "Options:"
    echo "h     Print help"
-   echo "r     Receptor map file (fld) directory"
-   echo "l     Ligands PDBQT"
+   echo "r     Receptor map file (fld) directory. All pocket folder should be inside this directory"
    echo "o     Output directory"
+   echo "l     Ligands PDBQT"
    echo "n     N runs"
    
 }
@@ -24,15 +25,15 @@ Help()
 ############################################################
 # Get the options
 
-while getopts ":hr:l:o:n:" option; do
+while getopts ":hl:n:" option; do
    case $option in
       h) # Print this help
          Help
          exit;;
-      r) # Receptor FLD directory
-         RECEPTOR_FLD=$OPTARG;;
       l) # Enter the Ligands PDBQT output path
          LIGANDS_PDBQT_PATH=$OPTARG;;
+      r) # Receptor FLD directory
+         RECEPTOR_FLD=$OPTARG;;
       o) # Output path
          OUTPUT_PATH=$OPTARG;;
       n) # NRuns
@@ -43,19 +44,16 @@ while getopts ":hr:l:o:n:" option; do
    esac
 done
 
-declare -a LIGANDS_PDBQT=($(ls ${LIGANDS_PDBQT_PATH}))
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#echo ${LIGANDS_PDBQT[@]}
+for i in {1..14}
+do
+    RECEPTOR_FLD_POCKET=${RECEPTOR_FLD}/sitio_${i}/
+    OUTPUT_PATH_POCKET=${OUTPUT_PATH}/sitio_${i}
 
-if [ ! -d $OUTPUT_PATH ]
-then
- mkdir $OUTPUT_PATH
-fi
+    echo "Doing for site ${i}"
 
-for LIGAND  in "${LIGANDS_PDBQT[@]}"
- do
-  echo "Docking ${LIGAND}"
-  /usr/local/bin/autodock_gpu_64wi -L ${LIGANDS_PDBQT_PATH}/${LIGAND} -M ${RECEPTOR_FLD}/*.maps.fld --nrun $NRUNS --resnam ${OUTPUT_PATH}/${LIGAND}
-  done
+    ${SCRIPT_PATH}/run_ad_gpu.sh -r ${RECEPTOR_FLD_POCKET} -l ${LIGANDS_PDBQT_PATH} -o ${OUTPUT_PATH_POCKET} -n 1000
 
-echo "Done!"
+    echo "Done for site ${i}"
+done
