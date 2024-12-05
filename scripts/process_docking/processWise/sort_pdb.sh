436ff33d17bcb1598fccbdffb7d19221a3051424
tcaceresm <caceres.tomas99@gmail.com>
2024-12-05 12:45:39 -0300

    updated file

--- scripts/process_docking/processWise/sort_pdb.sh
+++ scripts/process_docking/processWise/sort_pdb.sh
@@ -10,7 +10,7 @@
 Help() {
     echo "Sort ligands' conformations in PDB file based on binding energy."
     echo "Syntax: sort_pdb.sh [-h|d|i]."
-    echo "Requires already processed DLG file (process_dlg.sh)."
+    echo "Requires an already processed DLG file (process_dlg.sh)."
     echo "  The processed directory must be the same than "Processed DLG output directory"
     used by process_dlg.sh (-o flag)"
     echo "Options:"
