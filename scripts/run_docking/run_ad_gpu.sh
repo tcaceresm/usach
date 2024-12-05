edb32f3480120d583bed50c6f37b1451c6dbe550
tcaceresm <caceres.tomas99@gmail.com>
2024-12-05 11:58:23 -0300

    updated file

--- scripts/run_docking/run_ad_gpu.sh
+++ scripts/run_docking/run_ad_gpu.sh
@@ -50,8 +50,8 @@ mkdir -p ${OUTPUT_PATH}
 for LIGAND_NAME in "${LIGANDS_PDBQT[@]}"
 do
    LIGAND_NAME=$(basename ${LIGAND} .pdbqt)
-   echo "Docking ${LIGAND}"
-   /usr/local/bin/autodock_gpu_64wi -L ${LIGANDS_PDBQT_PATH}/${LIGAND_NAME}.pdbqt -M ${RECEPTOR_FLD}/*.maps.fld --nrun $NRUNS --resnam ${OUTPUT_PATH}/${LIGAND_NAME}
+   echo "Docking ${LIGAND_NAME}"
+   /usr/local/bin/autodock_gpu_64wi -L ${LIGANDS_PDBQT_PATH}/${LIGAND_NAME}.pdbqt -M ${RECEPTOR_FLD}/*.maps.fld --nrun ${NRUNS} --resnam ${OUTPUT_PATH}/${LIGAND_NAME}
 done
 
 echo "Done!"
